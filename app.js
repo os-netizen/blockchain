b5793e26fd82feff8b5d4a9d717bd393100160b8
omkar <omkarsgothankar@gmail.com>
2022-05-04 00:36:09 +0530

    first

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const port = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("./stylesheets/home");
+});
+
+app.get("/pricing", (req, res) => {
+  res.render("./stylesheets/pricing");
+});
+
+app.get("/proof", (req, res) => {
+  // res.send(req.params);
+  // res.render("proof");
+
+  const { contractAddress, tokenID } = req.query;
+  // console.log(contractAddress, tokenID);
+  res.render("./stylesheets/proofTry", {
+    contractAddress: contractAddress,
+    tokenID: tokenID,
+  });
+});
+
+app.get("/newProducts", (req, res) => {
+  res.render("./stylesheets/newProducts");
+});
+
+app.post("/newProducts", (req, res) => {
+  // res.render("./stylesheets/home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
