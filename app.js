9d7126ca00b85c0896774263c4a06cfaea7ccabb
omkar <omkarsgothankar@gmail.com>
2022-05-04 20:02:08 +0530

    third

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("path");
+const axios = require("axios");
+const ejsMate = require("ejs-mate");
+
+// import fetch from "node-fetch";
+
 const port = 3000;
 
+app.engine("ejs", ejsMate);
 app.set("views", "./views");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 // app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
@@ -19,29 +26,47 @@ app.get("/transfer", (req, res) => {
   res.render("./stylesheets/transfer");
 });
 
-app.get("/pricing", (req, res) => {
-  res.render("./stylesheets/pricing");
-});
-
-app.get("/proof", (req, res) => {
+app.get("/proof", async (req, res) => {
   // res.send(req.params);
   // res.render("proof");
 
   const { contractAddress, tokenID } = req.query;
-  // console.log(contractAddress, tokenID);
-  res.render("./stylesheets/proofTry");
+  const options = { method: "GET" };
+
+  const apiKey = "sCQ3HEkQrXzkClii7H_aw4hHGqEBcheP";
+  const baseURL = `https://eth-rinkeby.alchemyapi.io/v2/${apiKey}/getNFTMetadata`;
+  const tokenType = "erc721";
+
+  var config = {
+    method: "get",
+    url: `${baseURL}?contractAddress=${contractAddress}&tokenId=${tokenID}&tokenType=${tokenType}`,
+    headers: {},
+  };
+  let resp;
+  let add;
+  await axios(config)
+    .then((response) => {
+      // console.log(JSON.stringify(response.data, null, 2));
+      resp = response.data.metadata;
+      add = response.data.contract.address;
+      // console.log(resp["NFT series name"]);
+    })
+    .catch((error) => console.log(error));
+  res.render("./stylesheets/proofTry", {
+    image: resp.image,
+    noOfNFT: resp.noOfNFT,
+    companyName: resp.companyName,
+    address: add,
+    NFTname: resp["NFT series name"],
+  });
 });
 
 app.get("/newProducts", (req, res) => {
   res.render("./stylesheets/newProducts");
 });
 
-app.get("/mintToCustm", (req, res) => {
-  res.render("./stylesheets/mintToCustm");
-});
-
 app.post("/newProducts", (req, res) => {
-  // res.render("./stylesheets/home");
+  // res.render("./stylesheets/mintNew");
 });
 
 app.listen(port, () => {
--- views/javascripts/publish.js
+++ views/javascripts/publish.js
@@ -1,70 +1,76 @@
-import {abi, bytecode} from './abicode'
+import { abi, bytecode } from "./abicode";
 
+// const serverUrl = "https://jrhct3nepajw.usemoralis.com:2053/server";
+// const appId = "GzSUUx4WrVeCG5BEomiP0KdHc1aGhQmVDw5iElq7";
+// Moralis.start({ serverUrl, appId });
 
+// //login function
+// login = async () => {
+//   Moralis.authenticate().then(function (user) {
+//     console.log(user.get("ethAddress"));
+//     //   console.log(abi);
+//   });
+// };
 
+// //upload an image
+// uploadImage = async () => {
+//   const data = formFile.files[0];
+//   const file = new Moralis.File(data.name, data);
+//   await file.saveIPFS();
 
-const serverUrl = "https://jrhct3nepajw.usemoralis.com:2053/server";
-const appId = "GzSUUx4WrVeCG5BEomiP0KdHc1aGhQmVDw5iElq7";
-Moralis.start({ serverUrl, appId });
+//   console.log(file.ipfs(), file.hash());
 
-//login function
-login = async () => {
-  Moralis.authenticate().then(function (user) {
-    console.log(user.get("ethAddress"));
-    //   console.log(abi);
-  });
-};
+//   return file.ipfs();
+// };
 
-//upload an image
-uploadImage = async () => {
-  const data = formFile.files[0];
-  const file = new Moralis.File(data.name, data);
-  await file.saveIPFS();
+// //upload the metadata object
 
-  console.log(file.ipfs(), file.hash());
+// uploadMetaData = async (imageURL) => {
+//   const object = {
+//     companyName: document.getElementById("companyName").value,
+//     "NFT series name": document.getElementById("NFTprojectName").value,
+//     noOfNft: document.getElementById("noOfNFT").value,
+//     image: imageURL,
+//   };
+//   const file = new Moralis.File("file.json", {
+//     base64: btoa(JSON.stringify(object)),
+//   });
+//   await file.saveIPFS();
+//   console.log(file.ipfs(), file.hash());
+//   return file.ipfs();
+// };
 
-  return file.ipfs();
-};
+// //function to goooo
+// let ipfsMetaData = 0;
+// gogogo = async (e) => {
+//   e.preventDefault();
+//   const image = await uploadImage();
+//   ipfsMetaData = await uploadMetaData(image);
+//   // document.getElementById("publish").style.display = "none";
+//   // document.getElementById("mint").style.display = "block";
+//   // return ipfsMetaData;
+//   console.log(ipfsMetaData);
+//   const provider = new ethers.providers.Web3Provider(
+//     window.ethereum,
+//     "rinkeby"
+//   );
 
-//upload the metadata object
+//   await provider.send("eth_requestAccounts", []);
+//   const signer = provider.getSigner();
+//   console.log(signer);
 
-uploadMetaData = async (imageURL) => {
-  const object = {
-    companyName: document.getElementById("companyName").value,
-    "NFT series name": document.getElementById("NFTprojectName").value,
-    noOfNft: document.getElementById("noOfNFT").value,
-    image: imageURL,
-  };
-  const file = new Moralis.File("file.json", {
-    base64: btoa(JSON.stringify(object)),
-  });
-  await file.saveIPFS();
-  console.log(file.ipfs(), file.hash());
-  return file.ipfs();
-};
+//   const factory = new ContractFactory(abi, bytecode, signer);
 
-//function to goooo
-let ipfsMetaData = 0;
-gogogo = async () => {
-  const image = await uploadImage();
-  ipfsMetaData = await uploadMetaData(image);
-  document.getElementById("publish").style.display = "none";
-  document.getElementById("mint").style.display = "block";
-  return ipfsMetaData;
-};
+//   contract = await factory.deploy(name, symbol, ipfsMetaData);
+//   console.log(contract);
+//   const address = contract.address;
+//   const receipt = await contract.deployTransaction.wait();
+//   console.log(receipt);
 
+//   const NFTea = new ethers.Contract(address, abi, signer);
+//   console.log(object);
+//   const transaction = await contract.mint(recieverAddress, noOfNft);
+//   console.log(transaction);
+// };
 
-
-const mintNow = () => {
-
-        const provider = new ethers.providers.Web3Provider(window.ethereum, "rinkeby")
-
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner()
-
-    const factory = new ContractFactory(abi, bytecode, signer)
-
-       contract = await factory.deploy();
-};
-
-export {gogogo, mintNow};
+export { gogogo, login };
